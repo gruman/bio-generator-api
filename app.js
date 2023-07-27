3c390bd30df38ce5838e6822a57fa1e5d24f8932
Matthew Gruman <matthew.gruman@itdcanada.ca>
2023-07-26 21:59:04 -0700

    Backend

--- app.js
+++ app.js
@@ -1,11 +1,24 @@
-const express = require('express')
+const express = require('express');
 const app = express()
-const port = 3000
+var bodyParser = require('body-parser')
+const port = 8080
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET, POST, PUT, PATCH, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
+
+const publicRoutes = require('./routes/public')
+
+app.use('/', publicRoutes)
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
+
