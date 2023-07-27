ca4622535d38b2bbb32d8db7481cb05eec9e6c47
Matthew Gruman <matthew.gruman@itdcanada.ca>
2023-07-26 21:55:08 -0700

    First commit

--- app.js
+++ app.js
@@ -1,24 +1,11 @@
-const express = require('express');
+const express = require('express')
 const app = express()
-var bodyParser = require('body-parser')
-const port = 8080
+const port = 3000
 
-
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(bodyParser.json())
-
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET, POST, PUT, PATCH, DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  next();
-});
-
-const publicRoutes = require('./routes/public')
-
-app.use('/', publicRoutes)
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
-
