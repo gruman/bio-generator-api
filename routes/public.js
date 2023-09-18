69214db7fb15c12a6261d1b4d0040dd892f25177
Matthew Gruman <matthew.gruman@itdcanada.ca>
2023-09-18 01:20:46 -0700

    First Commit
    First commit

--- routes/public.js
+++ routes/public.js
@@ -5,7 +5,6 @@ const publicController = require('../controllers/public');
 const router = express.Router();
 
 // GET /feed/posts
-router.get('/', publicController.getBio);
-router.get('/:num', publicController.getBios);
+router.post('/scramble', publicController.getScrambled);
 
 module.exports = router;
